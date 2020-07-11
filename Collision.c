f249240ed8df3a28f146ae54150326a1feabc42c
StupidProgrammer <48482231+StupidPGM@users.noreply.github.com>
2020-07-11 11:03:45 +0300

    Update Collision.c

--- Collision.c
+++ Collision.c
@@ -1,41 +1,36 @@
 #include <stdio.h>
 
-void collision (struct player,struct enemy){
+struct build{
+	int x;
+	int y;
+	int width;
+	int height;
+}player, enemy;
+
+void collision (struct build player, struct build enemy){
 	if(player.x < enemy.x + enemy.width && player.x + player.width > enemy.x && player.y < enemy.y + enemy.height && player.y + player.height > enemy.y){
 		printf("it collision\n");
 	}else{
 		printf("not collision\n");
 	}
-struct playerbuild{
-	int x;
-	int y;
-	int width;
-	int height;
-}player;
-struct enemybuild{
-	int x;
-	int y;
-	int width;
-	int height;
-}enemy;
+}
 
 int main(){
 	printf("PlayerX: ");
-	scanf("%d",player.x);
+	scanf("%d",&player.x);
 	printf("PlayerY: ");
-	scanf("%d",player.y);
+	scanf("%d",&player.y);
 	
 	player.width = 50;
 	player.height = 50;
 	
 	printf("EnemyX: ");
-	scanf("%d",enemy.x);
-	printf("EnemyX: ");
-	scanf("%d",enemy.y);
+	scanf("%d",&enemy.x);
+	printf("EnemyY: ");
+	scanf("%d",&enemy.y);
 	
 	enemy.width = 50;
 	enemy.height = 50;
-	
 	
 	collision (player,enemy);
 	
